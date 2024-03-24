4d1fc7b90f9b8e9b8394c8e1bbc63c5ec4b0e2b8
JennyPopova <41321295+JennyPopova@users.noreply.github.com>
2024-03-24 14:54:11 +0100

    Create postCreateCommand.sh

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,11 @@
+pip install -r Labs/requirements.txt
+
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+sudo apt-get install ffmpeg
+
+sudo apt install libgstreamer1.0-0 \
+gstreamer1.0-plugins-base \
+gstreamer1.0-plugins-good \
+gstreamer1.0-plugins-bad \
+gstreamer1.0-plugins-ugly
